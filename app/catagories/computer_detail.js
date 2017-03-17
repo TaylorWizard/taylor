38b04a66e5a56375031e581e7765d93a01308dac
taylor <376977792@qq.com>
2017-03-17 18:12:33 +0800

    http angular-material

--- app/catagories/computer_detail.js
+++ app/catagories/computer_detail.js
@@ -1,29 +1,38 @@
 import angular from "angular";
+import template from "./com_view1_template.html";
 
 export default angular.module('computer_detail', [])
-  .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+  .config(['$urlRouterProvider', '$stateProvider', '$provide', function ($urlRouterProvider, $stateProvider, $provide) {
     //router
     //custom rule
-    $urlRouterProvider.rule(function($injector, $location) {
-        let path = $location.path();
-        let normalized = path.toLowerCase();
-        console.log("path:", path, ",normalized:", normalized, );
+    $urlRouterProvider.rule(function ($injector, $location) {
+      let path = $location.path();
+      let normalized = path.toLowerCase();
+      console.log("path:", path, ",normalized:", normalized);
     })
     $urlRouterProvider.otherwise('/detail')
     $urlRouterProvider.when('', '/computers/detail')
     $stateProvider.state('computers.detail', {
       url: '/detail',
-      // template: '<h1>this is computer_detail page</h1>',
+      //template: '<h1>this is computer_detail page</h1>',
       views: {
         view1: {
-          template: "this is computer detail view1"
+          template: template,
+          controller: 'ComController',
+          controllerAs: 'ComController'
         },
         view2: {
-          template: "this is computer detail view2"
+          template: "this is computer detail view2<div><div>"
         }
-      },
-      controller: function ($stateParams) {
-        console.log($stateParams);
       }
     })
   }])
+  .constant('comDetailConst', {
+    content: "hi there and computer_detail const!!!!"
+  })
+  .controller('ComController', ['$scope', function ($scope) {
+    console.log('this', this);
+    console.log('$scope', $scope);
+    this.name = "ComController"
+  }])
+
--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
-import angular from "angular";
-import router from "angular-ui-router"
+import angular from 'angular';
+import router from 'angular-ui-router'
+import ngAnimate from 'angular-animate';
+import ngAria from 'angular-aria';
+import ngMaterial from 'angular-material';
+
 
 import template from './helloTemplate.html';
 import toggleTemplate from "./toggleTemplate.html";
@@ -7,27 +11,63 @@ import segment_one from "./components/segment_one.js";
 import computer_detail from "./catagories/computer_detail.js"
 
 
-let app = angular.module("myApp", [router, segment_one.name, computer_detail.name], function () {
+let app = angular.module("myApp", [router, ngMaterial, segment_one.name, computer_detail.name], function () {
     console.log("here is my angular app!");
 })
-app.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+
+// app.provider('main', function () {
+//     let version;
+//     return {
+//         setVersion: function (value) {
+//             version = value;
+//         },
+//         $get: function () {
+//             return {
+//                 title: 'The mainProvider' + ',' + version
+//             }
+//         }
+//     }
+// })
+
+app.config(['$urlRouterProvider', '$stateProvider', 'comDetailConst', /*'mainProvider',*/ function ($urlRouterProvider, $stateProvider, comDetailConst/*, mainProvider*/) {
     $urlRouterProvider.otherwise('/computers')
     $urlRouterProvider.when('/computers', '/computers/detail')
     $stateProvider.state('computers', {
         url: '/computers',
         template: "this is computer page<div ui-view='view1'></div><div ui-view='view2'></dov>"
     })
+    // mainProvider.setVersion('v1.0.5');
+    console.log(comDetailConst.content);
 }])
 
-app.controller('myFirstController', ['$scope', function ($scope) {
+app.service('mainService', function () {
+    this.title = "The mainService";
+})
+
+app.factory('mainFactory', function () {
+    return {
+        title: "The mainFactory"
+    }
+})
+
+app.decorator('mainFactory', function ($delegate) {
+    console.log('decorator', $delegate.title);
+    return $delegate.title + " - blablabla";
+})
+
+
+app.controller('myFirstController', ['$scope', 'mainService', 'mainFactory',/*'main',*/ function ($scope, mainService, mainFactory/*,main*/) {
     $scope.firstName = "John";
     $scope.lastName = "Swizard";
     $scope.toggleText = "toggleText";
+    console.log("service", mainService.title);
+    console.log('factory', mainFactory);
+    //console.log('mainProvider--controller-inside', main.title);
 }])
 
 app.directive("greetDirective", ['$filter', function ($filter) {
     return {
-        restrict: 'ECMA',
+        restrict: 'EA',
         replace: true,
         template: template,
         transclude: true,
@@ -40,7 +80,7 @@ app.directive("greetDirective", ['$filter', function ($filter) {
 
 app.directive("toggleTemplate", [function () {
     return {
-        restrict: 'ECMA',
+        restrict: 'EA',
         replace: true,
         template: toggleTemplate,
         transclude: true,
@@ -54,6 +94,11 @@ app.directive("toggleTemplate", [function () {
             }
         }
     }
+}])
+
+
+app.run(['$rootScope', function ($rootScope) {
+    console.log("app is done loading all modules");
 }])
 
 document.addEventListener('DOMContentLoaded', () => angular.bootstrap(document.querySelector("#myApp"), [app.name]))
